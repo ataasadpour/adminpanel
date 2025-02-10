81f08ad968ceec6d58cf591194058e64429e1a67
ataasadpour <ataasadpour661@gmail.com>
2025-02-11 01:45:06 +0330

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@
 var themebut = document.getElementById("theme-but");
 var thembody = document.getElementById("body");
 
+if (localStorage.getItem("theme")) {
+    thembody.className = localStorage.getItem("theme");
+}
+
 themebut.addEventListener("click", () => {
     if (thembody.className === "dark") {
         thembody.className = "light";
     } else {
         thembody.className = "dark";
     }
+    
+    localStorage.setItem("theme", thembody.className);
 });
 
 // load
@@ -17,3 +23,4 @@ window.addEventListener('load', function () {
     document.querySelector('.loader-wrapper').style.display = 'none';
     document.querySelector('.main-all').style.display = 'block';
 });
+
