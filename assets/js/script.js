310912dedcb13d4b84c72b19eac2c7d870cc1c69
ataasadpour <ataasadpour661@gmail.com>
2025-02-13 13:31:01 +0330

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,27 @@ window.addEventListener('load', function () {
     document.querySelector('.loader-wrapper').style.display = 'none';
     document.querySelector('.main-all').style.display = 'block';
 });
+
+
+// chart-1
+
+var chart1 = document.getElementById('chart1').getContext('2d');
+    var myChart = new Chart(chart1, {
+        type: 'line',
+        data: {
+            labels: ['بازدید','فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر' ,'آبان','آذر','دی','بهمن' , 'اسفند'],
+            datasets: [{
+                label: 'بازدیدها ماه',
+                data: [null,65, 59, 80, 81, 56, 55, 40 , 50,10,25,32 , 15],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
